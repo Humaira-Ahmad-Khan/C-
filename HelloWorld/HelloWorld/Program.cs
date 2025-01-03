885fc486c5408883706420b984d973f1b0e70ec8
Humaira-Ahmad-Khan <digitalartistctn@gmail.com>
2025-01-03 18:38:48 +0500

    Helloworld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class helloWorld 
+{
+    static void Call()
+    {
+        Typing();
+    }
+    static void Typing()
+    {
+        Console.WriteLine("This is HelloWorld"+" "+ 25);
+    }
+}
